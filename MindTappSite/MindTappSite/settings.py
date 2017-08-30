406011a2154e12e99c6e15ff6c742b754e0227a2
jjkjiang <jerryjiekaijiang@gmail.com>
2017-08-29 17:02:25 -0700

    added potential settings for production

--- MindTappSite/MindTappSite/settings.py
+++ MindTappSite/MindTappSite/settings.py
@@ -75,12 +75,20 @@ WSGI_APPLICATION = 'MindTappSite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+is_prod = false
+
+
+if is_prod:
+    from . import db_settings
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+
+
 
 
 # Password validation
@@ -123,4 +131,4 @@ STATIC_URL = '/static/'
 
 
 # Storing private information like keys, email backend, etc
-from .credentials import *
+from .email_settings import *
