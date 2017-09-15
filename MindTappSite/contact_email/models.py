61d2c6a401efbd6d55ead285f06e2032cbb056a7
root <root@ip-172-31-34-240.us-west-2.compute.internal>
2017-09-15 18:04:13 +0000

    favicon added

--- MindTappSite/contact_email/models.py
+++ MindTappSite/contact_email/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Recipient(models.Model):
-    email = models.EmailField()
+    email = models.EmailField(help_text="Upon hitting contact me on the main page, will send an email to all emails listed here.")
 
     def __str__(self):
         return self.email
