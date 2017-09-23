9685413a8d647a1a127a0c52580a2ddbd8b40265
jjkjiang <jerryjiekaijiang@gmail.com>
2017-09-22 20:16:31 -0700

    deleted accidental push

--- MindTappSite/api/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Survey(models.Model):
-    public = models.BooleanField()
-    title = models.TextField()
-    description = models.TextField()
-    active = models.BooleanField()
--- MindTappSite/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Survey
-
-class SurveySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Survey
-        fields = ('id', 'title', 'public', 'description', 'active')
-
