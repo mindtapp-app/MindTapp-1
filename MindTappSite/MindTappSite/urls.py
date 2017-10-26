14c5e07cf4735811cd0682bba626285eb9a31220
Franklin Yang <fyang016@ucr.edu>
2017-10-26 16:28:23 -0700

    changed urls.py

--- MindTappSite/MindTappSite/urls.py
+++ MindTappSite/MindTappSite/urls.py
@@ -14,13 +14,51 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+
+from django.urls import reverse_lazy
+
 from django.contrib import admin
 from . import views
-from django.contrib.auth.views import PasswordResetView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)	
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 
 urlpatterns = [
+    
+    url(r'^login/$', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    url(r'^logout/$', LogoutView.as_view(next_page=reverse_lazy('login')), name='logout'),
+
+    url(r'^password_change/$',
+        PasswordChangeView.as_view(template_name='accounts/password_change_form.html'),
+        name='password_change'),
+    url(r'^password_change/done/$',
+        PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'),
+        name='password_change_done'),
+
+    url(r'^password_reset/$',
+        PasswordResetView.as_view(template_name='accounts/password_reset_form.html'),
+        name='password_reset'),
+    url(r'^password_reset/done/$',
+        PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
+        name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
+        name='password_reset_confirm'),
+    url(r'^reset/done/$',
+        PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+        name='password_reset_complete'),
+    
+    
+    
     url(r'^admin/?', admin.site.urls),
     url(r'^$', views.index, name='index'),
     #url(r'^register/?', views.RegistrationView.as_view()),
